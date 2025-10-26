a39c82e7b31dd02ddf3fc05953819b05dc24406a
Doga Fincan <doga@icloud.com>
2025-10-26 22:46:35 +0100

    Fix linting issues in Pomodoro page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,7 @@
+if (typeof globalThis.structuredClone !== "function") {
+  globalThis.structuredClone = (value) => JSON.parse(JSON.stringify(value));
+}
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
